468cde0a7171498c36552d32274969cb9eb489aa
Hatvtv <Hatvtv@192.168.1.39>
2017-10-08 13:27:53 +0200

    primer save

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        class BaseClass {
+   function __construct() {
+       print " Constructor Base Class\n";
+   }
+    function suma($a,$b){
+       return $a+$b;
+   }
+   
+}
+        
+        
+        
+        
+        
+        ?>
+    </body>
+</html>
